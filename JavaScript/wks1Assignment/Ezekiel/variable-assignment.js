8db4dccc31746c860de508bbc60f2b45cb2b2c00
DEBORAH ADEGBOYE <adegboyedeborahtaiwo@gmail.com>
2023-01-13 04:36:12 -0800

    Deborah Assignment

--- JavaScript/wks1Assignment/Ezekiel/variable-assignment.js
+++ JavaScript/wks1Assignment/Ezekiel/variable-assignment.js
@@ -1,1 +1,22 @@
 // this is my assignment solution
+
+let admin, name; // can declare two variables at once
+
+name = "John";
+
+admin = name;
+
+alert(admin); // "John"
+
+
+
+let Planet = "Earth";
+
+let UserName = "John"; 
+
+
+
+const BIRTHDAY = '18.04.1982'; // make birthday uppercase?
+const AGE = someCode(BIRTHDAY); // make age uppercase?
+
+
