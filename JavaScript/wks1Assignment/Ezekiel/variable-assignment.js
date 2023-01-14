52b5b89f6dede253372e78b9f6f4438065459ead
DEBORAH ADEGBOYE <adegboyedeborahtaiwo@gmail.com>
2023-01-14 03:39:23 -0800

    Assignment done

--- JavaScript/wks1Assignment/Ezekiel/variable-assignment.js
+++ JavaScript/wks1Assignment/Ezekiel/variable-assignment.js
@@ -13,10 +13,24 @@ alert(admin); // "John"
 let Planet = "Earth";
 
 let UserName = "John"; 
-
-
-
 const BIRTHDAY = '18.04.1982'; // make birthday uppercase?
 const AGE = someCode(BIRTHDAY); // make age uppercase?
+
+
+let bets = "crown: 0, anchor: 0, heart:0, spade:0, club:0, diamond:0"
+let totalBet = rand (1,funds)
+
+{
+    if(totalBet === '7?'){
+        totalBet = "funds";
+    }else{
+        totalBet = "randomly distribute totalBet";
+    } if (totalBet = "funds"){
+
+    }
+}
+
+ 
+
 
 
